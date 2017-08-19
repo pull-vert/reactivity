d1ddcee218494d33991deebc4c265622a4c3be11
pull-vert <frederic.montariol@gmail.com>
2017-08-20 00:03:58 +0200

    refactoring of comment in module-info

--- src/main/kotlin/module-info.java
+++ src/main/kotlin/module-info.java
@@ -1,30 +1,5 @@
-/*
- * Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 /**
- * Defines the high-level HTTP and WebSocket API.
+ * Defines high-level HTTP2 Server API.
  * {@Incubating}
  *
  * @moduleGraph
