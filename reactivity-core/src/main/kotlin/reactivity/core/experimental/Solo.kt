9153652db818bfc3b923e86c52d6b0040b9c3d4a
pull-vert <frederic.montariol@gmail.com>
2017-09-07 23:00:55 +0200

    allowing to create a Solo instance from a suspending block

--- reactivity-core/src/main/kotlin/reactivity/core/experimental/Solo.kt
+++ reactivity-core/src/main/kotlin/reactivity/core/experimental/Solo.kt
@@ -1,7 +1,19 @@
 package reactivity.core.experimental
 
+import kotlinx.coroutines.experimental.channels.ProducerScope
+import kotlinx.coroutines.experimental.reactive.publish
 import org.reactivestreams.Publisher
+import kotlin.coroutines.experimental.CoroutineContext
+
+fun <T> solo(
+        context: CoroutineContext,
+        block: suspend ProducerScope<T>.() -> Unit
+): Solo<T> = SoloImpl(publish(context, block))
 
 abstract class Solo<T> : Publisher<T> {
+
+}
+
+internal class SoloImpl<T> internal constructor(override val delegate: Publisher<T>) : Solo<T>(), PublisherDelegated<T> {
 
 }
