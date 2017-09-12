0862e07e1f739460e2d2e5ae1fddd2ae1f650877
frederic.montariol <frederic.montariol@adventiel.fr>
2017-09-12 16:01:57 +0200

    ideas to implement Solo

--- reactivity-core/src/main/kotlin/reactivity/core/experimental/Solo.kt
+++ reactivity-core/src/main/kotlin/reactivity/core/experimental/Solo.kt
@@ -5,6 +5,10 @@ import kotlinx.coroutines.experimental.reactive.publish
 import org.reactivestreams.Publisher
 import kotlin.coroutines.experimental.CoroutineContext
 
+/** TODO implement the Solo Publisher
+ * @see https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.experimental/async.html
+ * @see https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.experimental/-completable-deferred/index.html
+ */
 fun <T> solo(
         context: CoroutineContext,
         block: suspend ProducerScope<T>.() -> Unit
