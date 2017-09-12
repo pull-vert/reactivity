caa6ccfd78c0ef8a29e854c2e7d5a65efe512033
pull-vert <frederic.montariol@gmail.com>
2017-09-12 22:57:17 +0200

    MultiChannel is now public to be exposed to abstract function Multi.groupBy

--- reactivity-core/src/main/kotlin/reactivity/core/experimental/MultiChannel.kt
+++ reactivity-core/src/main/kotlin/reactivity/core/experimental/MultiChannel.kt
@@ -4,5 +4,4 @@ import kotlinx.coroutines.experimental.channels.Channel
 import kotlinx.coroutines.experimental.reactive.asPublisher
 import kotlin.coroutines.experimental.CoroutineContext
 
-internal class MultiChannel<T> internal constructor(coroutineContext: CoroutineContext, val channel: Channel<T>) : MultiImpl<T>(channel.asPublisher(coroutineContext)) {
-}
+class MultiChannel<T> internal constructor(coroutineContext: CoroutineContext, val channel: Channel<T>) : MultiImpl<T>(channel.asPublisher(coroutineContext))
