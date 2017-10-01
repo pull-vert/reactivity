99b7efe462edde60b93434d91cfb34f3cad25090
pull-vert <frederic.montariol@gmail.com>
2017-10-02 00:36:21 +0200

    DEFAULT_SCHEDULER becomes public for access from other modules

--- core/reactivity-core/src/main/kotlin/reactivity/experimental/PublisherCommons.kt
+++ core/reactivity-core/src/main/kotlin/reactivity/experimental/PublisherCommons.kt
@@ -4,7 +4,7 @@ package reactivity.experimental
 /**
  * The default scheduler used for instantiation of [Multi] and [Solo]
  */
-internal val DEFAULT_SCHEDULER = Schedulers.defaultDispatcher()
+val DEFAULT_SCHEDULER = Schedulers.defaultDispatcher()
 
 /**
  * Common functions for [Multi] and [Solo]
