95d477a3f81629673458529926cecfbd0fe11377
pull-vert <frederic.montariol@gmail.com>
2017-10-01 14:57:53 +0200

    Big project structure changes anticipating new modules to come

--- core/reactivity-jre8/src/test/java/reactivity/experimental/MultiJavaTest.java
+++ core/reactivity-jre8/src/test/java/reactivity/experimental/MultiJavaTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class MultiJavaTest {
     @Test
     public void rangeJavaTest() {
-        MultiBuilder.fromRange(1, 3, Schedulers.emptyThreadContext())
+        MultiBuilder.fromRange(1, 3)
                 .subscribe((value) -> {
                     System.out.println(value);
                     return null;
