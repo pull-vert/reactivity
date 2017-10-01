cfc273d612df1a44ea65de87a3e0eb54f37aa8a2
pull-vert <frederic.montariol@gmail.com>
2017-10-01 09:35:19 +0200

    remove Scheduler param from java test

--- reactivity-core/src/test/java/reactivity/experimental/MultiJavaTest.java
+++ reactivity-core/src/test/java/reactivity/experimental/MultiJavaTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class MultiJavaTest {
     @Test
     public void rangeJavaTest() {
-        MultiBuilder.fromRange(1, 3, Schedulers.emptyThreadContext())
+        MultiBuilder.fromRange(1, 3)
                 .subscribe(new Function1<Integer, Unit>() {
                     @Override
                     public Unit invoke(Integer value) {
