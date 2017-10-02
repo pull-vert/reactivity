b7ed23cef259cf444a5397e846cfaf5ecbfdcf91
pull-vert <frederic.montariol@gmail.com>
2017-10-02 23:37:01 +0200

    rename test and add comment

--- core/reactivity-core-jdk8/src/test/java/reactivity/experimental/jdk8/MultiJdk8Test.java
+++ core/reactivity-core-jdk8/src/test/java/reactivity/experimental/jdk8/MultiJdk8Test.java
@@ -6,7 +6,7 @@ import reactivity.experimental.MultiBuilder;
 public class MultiJdk8Test {
     @Test
     public void rangeJavaTest() {
-        // shows that it works with a nice jdk8 lambda !
+        // Works with a nice jdk8 lambda !
         MultiBuilder.fromRange(1, 3)
                 .subscribe((value) -> {
                     System.out.println(value);
--- core/reactivity-core/src/test/java/reactivity/experimental/MultiJdk6Test.java
+++ core/reactivity-core/src/test/java/reactivity/experimental/MultiJdk6Test.java
@@ -4,10 +4,11 @@ import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 import org.junit.Test;
 
-public class MultiJavaTest {
+public class MultiJdk6Test {
     @Test
     public void rangeJavaTest() {
         MultiBuilder.fromRange(1, 3)
+                // Works with an anonymous function for jdk6 and jdk6
                 .subscribe(new Function1<Integer, Unit>() {
                     @Override
                     public Unit invoke(Integer value) {
