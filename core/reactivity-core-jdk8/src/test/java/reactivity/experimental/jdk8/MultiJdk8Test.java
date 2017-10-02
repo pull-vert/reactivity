7bbdbea58bace2b7b9cdd65350a542bb46809be8
pull-vert <frederic.montariol@gmail.com>
2017-10-02 22:55:07 +0200

    rename test and add comment

--- core/reactivity-core-jdk8/src/test/java/reactivity/experimental/jdk8/MultiJdk8Test.java
+++ core/reactivity-core-jdk8/src/test/java/reactivity/experimental/jdk8/MultiJdk8Test.java
@@ -3,9 +3,10 @@ package reactivity.experimental.jdk8;
 import org.junit.Test;
 import reactivity.experimental.MultiBuilder;
 
-public class MultiJavaTest {
+public class MultiJdk8Test {
     @Test
     public void rangeJavaTest() {
+        // shows that it works with a nice jdk8 lambda !
         MultiBuilder.fromRange(1, 3)
                 .subscribe((value) -> {
                     System.out.println(value);
