319f4274a10e92542fb0a0b5aea876e7bca3db71
pull-vert <frederic.montariol@gmail.com>
2017-11-25 16:30:59 +0100

    switch expected values for atomicFU

--- core/reactivity-common/src/main/kotlin/reactivity/experimental/expectKotlinxAtomicFU.kt
+++ core/reactivity-common/src/main/kotlin/reactivity/experimental/expectKotlinxAtomicFU.kt
@@ -1,12 +1,10 @@
 package reactivity.experimental
 
-expect fun <T> atomic(initial: T): AtomicRef<T>
-
 expect class AtomicRef<T> {
     @Volatile
     var value: T
     fun compareAndSet(expect: T, update: T): Boolean
     fun getAndSet(value: T): T
-    override fun toString(): String
-
 }
+
+expect fun <T> atomic(initial: T): AtomicRef<T>
--- core/reactivity-jdk6/src/main/kotlin/reactivity/experimental/actualKotlinxAtomicFUJdk6.kt
+++ core/reactivity-jdk6/src/main/kotlin/reactivity/experimental/actualKotlinxAtomicFUJdk6.kt
@@ -2,6 +2,6 @@ package reactivity.experimental
 
 import kotlinx.atomicfu.AtomicRef
 
-actual fun <T> atomic(initial: T): AtomicRef<T> = kotlinx.atomicfu.atomic(initial)
-
 actual typealias AtomicRef<T> = AtomicRef<T>
+
+actual fun <T> atomic(initial: T): AtomicRef<T> = kotlinx.atomicfu.atomic(initial)
