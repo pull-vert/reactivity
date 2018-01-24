3c0139b6b6500d0b9e964d4e372d6db4ee845459
pull-vert <frederic.montariol@gmail.com>
2018-01-25 00:06:29 +0100

    spsc6 is working (just end check)

--- bench/reactivity-stream-benchmarks/src/test/kotlin/sourceCollector/SourceCollectorTest.kt
+++ bench/reactivity-stream-benchmarks/src/test/kotlin/sourceCollector/SourceCollectorTest.kt
@@ -4,7 +4,6 @@ import benchmark.N
 import benchmark.isGood
 import kotlinx.coroutines.experimental.newSingleThreadContext
 import kotlinx.coroutines.experimental.runBlocking
-import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -20,121 +19,121 @@ class SourceCollectorTest {
         assertEquals(446448416, value)
     }
 
-    @Test
-    fun testSourceCollectorQuick() = runBlocking {
-        val value = SourceCollector
-                .range(1, 10)
-                .async(newSingleThreadContext("testSourceCollectorQuick"), buffer = 8)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorQuick : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(12, value)
-    }
-
-    @Test
-    fun testSourceCollectorQuick2() = runBlocking {
-        val value = SourceCollector
-                .range(1, 10)
-                .async2(newSingleThreadContext("testSourceCollectorQuick2"), buffer = 8)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorQuick2 : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(12, value)
-    }
-
-    @Test
-    @Ignore
-    fun testSourceCollectorQuick3() = runBlocking {
-        val value = SourceCollector
-                .range(1, 10)
-                .async3(newSingleThreadContext("testSourceCollectorQuick3"), buffer = 8)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorQuick3 : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(12, value)
-    }
-
-    @Test
-    fun testSourceCollectorQuick4() = runBlocking {
-        val value = SourceCollector
-                .range(1, 10)
-                .async4(newSingleThreadContext("testSourceCollectorQuick4"), buffer = 8)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorQuick4 : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(12, value)
-    }
-
-    @Test
-    fun testSourceCollectorQuick5() = runBlocking {
-        val value = SourceCollector
-                .range(1, 10)
-                .async5(newSingleThreadContext("testSourceCollectorQuick5"), buffer = 8)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorQuick5 : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(12, value)
-    }
-
-    @Test
-    @Ignore
-    fun testSourceCollectorAsync() = runBlocking {
-        val value = SourceCollector
-                .range(1, N)
-                .async(newSingleThreadContext("testSourceCollectorAsync"), buffer = 64)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorAsync : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(446448416, value)
-    }
-
-    @Test
-    @Ignore
-    fun testSourceCollectorAsync2() = runBlocking {
-        val value = SourceCollector
-                .range(1, N)
-                .async2(newSingleThreadContext("testSourceCollectorAsync2"), buffer = 128)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorAsync2 : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(446448416, value)
-    }
-
-    @Test
-    @Ignore
-    fun testSourceCollectorAsync3() = runBlocking {
-        val value = SourceCollector
-                .range(1, N)
-                .async3(newSingleThreadContext("testSourceCollectorAsync3"), buffer = 128)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorAsync3 : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(446448416, value)
-    }
-
-    @Test
-    @Ignore
-    fun testSourceCollectorAsync4() = runBlocking {
-        val value = SourceCollector
-                .range(1, N)
-                .async4(newSingleThreadContext("testSourceCollectorAsync4"), buffer = 128)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorAsync4 : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(446448416, value)
-    }
-
-    @Test
-    @Ignore
-    fun testSourceCollectorAsync5() = runBlocking {
-        val value = SourceCollector
-                .range(1, N)
-                .async5(newSingleThreadContext("testSourceCollectorAsync5"), buffer = 128)
-                .filter { it.isGood() }
-                .fold(0, { a, b -> a + b })
-        println("testSourceCollectorAsync5 : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(446448416, value)
-    }
+//    @Test
+//    fun testSourceCollectorQuick() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, 10)
+//                .async(newSingleThreadContext("testSourceCollectorQuick"), buffer = 8)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorQuick : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(12, value)
+//    }
+//
+//    @Test
+//    fun testSourceCollectorQuick2() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, 10)
+//                .async2(newSingleThreadContext("testSourceCollectorQuick2"), buffer = 8)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorQuick2 : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(12, value)
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun testSourceCollectorQuick3() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, 10)
+//                .async3(newSingleThreadContext("testSourceCollectorQuick3"), buffer = 8)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorQuick3 : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(12, value)
+//    }
+//
+//    @Test
+//    fun testSourceCollectorQuick4() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, 10)
+//                .async4(newSingleThreadContext("testSourceCollectorQuick4"), buffer = 8)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorQuick4 : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(12, value)
+//    }
+//
+//    @Test
+//    fun testSourceCollectorQuick5() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, 10)
+//                .async5(newSingleThreadContext("testSourceCollectorQuick5"), buffer = 8)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorQuick5 : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(12, value)
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun testSourceCollectorAsync() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, N)
+//                .async(newSingleThreadContext("testSourceCollectorAsync"), buffer = 64)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorAsync : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(446448416, value)
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun testSourceCollectorAsync2() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, N)
+//                .async2(newSingleThreadContext("testSourceCollectorAsync2"), buffer = 128)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorAsync2 : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(446448416, value)
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun testSourceCollectorAsync3() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, N)
+//                .async3(newSingleThreadContext("testSourceCollectorAsync3"), buffer = 128)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorAsync3 : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(446448416, value)
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun testSourceCollectorAsync4() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, N)
+//                .async4(newSingleThreadContext("testSourceCollectorAsync4"), buffer = 128)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorAsync4 : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(446448416, value)
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun testSourceCollectorAsync5() = runBlocking {
+//        val value = SourceCollector
+//                .range(1, N)
+//                .async5(newSingleThreadContext("testSourceCollectorAsync5"), buffer = 128)
+//                .filter { it.isGood() }
+//                .fold(0, { a, b -> a + b })
+//        println("testSourceCollectorAsync5 : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(446448416, value)
+//    }
 
     @Test
     fun testSourceCollectorAsync6() = runBlocking {
--- bench/reactivity-stream-benchmarks/src/test/kotlin/sourceInline/SourceInlineTest.kt
+++ bench/reactivity-stream-benchmarks/src/test/kotlin/sourceInline/SourceInlineTest.kt
@@ -1,36 +1,27 @@
 package sourceInline
 
-import benchmark.N
-import benchmark.isGood
-import channel.*
-import kotlinx.coroutines.experimental.newSingleThreadContext
-import kotlinx.coroutines.experimental.runBlocking
-import org.junit.Ignore
-import org.junit.Test
-import kotlin.test.assertEquals
-
 class SourceInlineTest {
-    @Test
-    @Ignore
-    fun testSourceInlineDeep() = runBlocking {
-        val value = SourceInline
-                .range(1, N)
-                .async(newSingleThreadContext("test"), buffer = 128)
-                .filter2 { it.isGood() }
-                .fold2(0, { a, b -> a + b })
-        println("testSourceInlineDeep : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(446448416, value)
-    }
-
-    @Test
-    @Ignore
-    fun testSourceInlineDeepQuick() = runBlocking {
-        val value = SourceInline
-                .range(1, 10)
-                .async(newSingleThreadContext("test"), buffer = 128)
-                .filter2 { it.isGood() }
-                .fold2(0, { a, b -> a + b })
-        println("testSourceInlineDeepQuick : value = $value run on ${Thread.currentThread().name}")
-        assertEquals(12, value)
-    }
+//    @Test
+//    @Ignore
+//    fun testSourceInlineDeep() = runBlocking {
+//        val value = SourceInline
+//                .range(1, N)
+//                .async(newSingleThreadContext("test"), buffer = 128)
+//                .filter2 { it.isGood() }
+//                .fold2(0, { a, b -> a + b })
+//        println("testSourceInlineDeep : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(446448416, value)
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun testSourceInlineDeepQuick() = runBlocking {
+//        val value = SourceInline
+//                .range(1, 10)
+//                .async(newSingleThreadContext("test"), buffer = 128)
+//                .filter2 { it.isGood() }
+//                .fold2(0, { a, b -> a + b })
+//        println("testSourceInlineDeepQuick : value = $value run on ${Thread.currentThread().name}")
+//        assertEquals(12, value)
+//    }
 }
